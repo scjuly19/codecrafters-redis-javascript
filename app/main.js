087ab4ab0cbd98cbcd9c035de38dd455a5878f71
scjuly19 <scjuly69@gmail.com>
2024-02-07 22:52:49 +0530

    pass stage 5

--- app/main.js
+++ app/main.js
@@ -1,12 +1,23 @@
 const net = require("net");
+const { Buffer } = require("node:buffer");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.on("data",(data)=>{
-    connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    const clientReq = data.toString().trim().split("\r\n");
+    const cmd = clientReq[2].toLowerCase();
+    const msg = clientReq[4]
+    switch (cmd) {
+      case "echo":
+        connection.write(`+${msg}\r\n`)
+        break;
+      default:
+        connection.write("+PONG\r\n");
+        break
+    }
   })
 });
 
